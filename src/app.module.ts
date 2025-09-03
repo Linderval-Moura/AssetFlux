4744c2489f074c3892f84ea091ad9d85f0a3bbdd
Linderval-Moura <lindervalmatias@gmail.com>
2025-09-03 20:26:18 -0300

    feat: generate core modules and components using Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ImagesModule } from './images/images.module';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
+import { ImagesModule } from './images/images.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ImagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
