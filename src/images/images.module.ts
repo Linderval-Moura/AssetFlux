ffbba583ae3efcdbb470cca41627c4f97aa3ae7a
Linderval-Moura <lindervalmatias@gmail.com>
2026-02-03 07:01:10 -0300

    chore(infra): setup redis container and configure cache module dependencies

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -3,9 +3,26 @@ import { ImagesController } from './images.controller';
 import { ImagesService } from './images.service';
 import { ProvidersModule } from '../providers.module';
 import { UsersModule } from '../users/users.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          socket: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: +configService.get<number>('REDIS_PORT'),
+          },
+          ttl: +configService.getOrThrow('REDIS_TTL'),
+        }),
+      }),
+      inject: [ConfigService],
+    }),
+    
     ProvidersModule, 
     UsersModule,
   ],
